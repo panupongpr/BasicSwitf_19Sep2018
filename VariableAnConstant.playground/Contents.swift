3db68ce4d16077b864ca744ede92abc01f424d97
ton <panupong.pspd@gmail.com>
2018-09-19 10:29:23 +0700

    Variable and Constant

---
+++ VariableAnConstant.playground/Contents.swift
@@ -0,0 +1,35 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+//Variable and Constant
+//String
+var naemString = "Ton Panupong" //นี่คือการประกาศค่าคงที่
+let surnameString : String = "Prasoppakdee" // นี่คือ Constant Explicit by Type
+
+naemString = "Doramon"
+//Integer
+let number1Int = 100            // Implicit Integer Type
+let number2Int: Int = 500       // Explicit Config Data Type
+
+var answer1Int: Int = number1Int + number2Int
+
+//Double
+let number3Dou = 12.23 // Implicit Double Type
+let number4Dou: Double = 100 // Explicit Double Type
+
+let answer2Dou: Double = number3Dou - number4Dou
+
+//Boolean
+let statusABool = true
+
+let statusBBol: Bool = false
+
+let answwer3Bool = !statusBBol
+
+
+
+
+
+
+
