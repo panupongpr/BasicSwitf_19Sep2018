2f540ffe05715188a182bdf3e11858c8d23f6613
ton <panupong.pspd@gmail.com>
2018-09-19 13:44:54 +0700

    Basic Array Success

---
+++ ArrayAndDictionary.playground/Contents.swift
@@ -0,0 +1,36 @@
+import UIKit
+
+var numberInts = [11,22,33,44]
+var nameString = ["AAA","BBB"]
+
+//length
+let lengthNumberInts = numberInts.count
+
+let lengthNameStrings = nameString.count
+
+//Add member on Array
+
+numberInts.append(55)
+
+nameString.append("CCC")
+
+//Deletet member onArray
+
+numberInts.removeLast()
+
+numberInts
+
+numberInts.removeFirst()
+
+numberInts
+
+numberInts.remove(at: 1)
+
+numberInts
+
+numberInts.append(5)
+
+numberInts.append(55)
+
+numberInts.sort()
+
