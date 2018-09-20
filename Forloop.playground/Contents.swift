50a08e2384da63b0a30998b79856aafaca05da3b
ton <panupong.pspd@gmail.com>
2018-09-20 09:13:23 +0700

    For Loop

---
+++ Forloop.playground/Contents.swift
@@ -0,0 +1,20 @@
+import UIKit
+
+let numberInts = [11,22,33,44,55,66,77,88,99]
+
+
+for myNumber in numberInts {
+    
+    print("myNumber ==>\(myNumber)")
+
+}
+
+let nameString = ["AAA","BBB","CCC"]
+
+for myName in nameString {
+    print("Mr.\(myName)")
+}
+
+
+
+
