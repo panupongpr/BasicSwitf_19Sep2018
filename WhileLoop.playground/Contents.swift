72a0479743cbe41fe67b5408157c66ab7cbb22bf
ton <panupong.pspd@gmail.com>
2018-09-20 09:23:21 +0700

    while

---
+++ WhileLoop.playground/Contents.swift
@@ -0,0 +1,18 @@
+import UIKit
+
+let timesInt: Int = 12
+
+let factorInt: Int = 2
+
+var countInt = 1
+
+//Condition is True Do in Loop
+
+while countInt <= timesInt {
+    
+    print("\(factorInt) X \(countInt) ==> \(factorInt*countInt)")
+    
+    countInt += 1
+}
+
+
